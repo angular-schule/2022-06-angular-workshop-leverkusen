ff4052ae5cba9a91cf0301fdf03914c564bc48aa
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-06-20 14:59:03 +0200

    showing books as string

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+
+  books = ['Angular', 'AngularJS', 'AngularJS'];
+}
