94ed03a1470afb986c1fb3d74c1d525dc7ef8199
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-21 10:29:21 +0200

    routing: router is lazy 😱

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'br-book-details',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    console.log(route.snapshot.url);
+  }
 
   ngOnInit(): void {
   }
