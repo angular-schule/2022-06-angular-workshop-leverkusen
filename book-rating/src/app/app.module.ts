cb4e82ec582673f464c760904abc1ea7c971043f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-07-22 14:19:07 +0200

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools ng add @ngrx/schematics --default-collection ng g @ngrx/schematics:feature books/store/book --module books/books --api --defaults

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './books/book/book.component';
 
@@ -18,7 +22,10 @@ import { BooksModule } from './books/books.module';
     BrowserModule,
     AppRoutingModule,
     BooksModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- rxjs-playground/src/app/app.module.ts
+++ rxjs-playground/src/app/app.module.ts
@@ -7,6 +7,10 @@ import localeDe from '@angular/common/locales/de';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ExercisesModule } from './exercises/exercises.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
@@ -16,6 +20,9 @@ import { ExercisesModule } from './exercises/exercises.module';
       { path: '', redirectTo: 'exercises', pathMatch: 'full' },
     ]),
     ExercisesModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'de' }],
   bootstrap: [AppComponent],
